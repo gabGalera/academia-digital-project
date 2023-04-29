af12d67ace199bbc08e1169ec47e6b8e03f3c1bd
Gabriel Galera <gabgalera@hotmail.com>
2023-04-29 15:49:11 -0300

    feat: AvaliacaoFisicaUpdateForm class

---
+++ src/main/java/dio/academiaDigital/project/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -0,0 +1,6 @@
+package dio.academiaDigital.project.entity.form;
+
+public class AvaliacaoFisicaUpdateForm {
+    private double peso;
+    private double altura;
+}
--- src/main/java/dio/academiaDigital/project/service/IAvaliacaoFisica.java
+++ src/main/java/dio/academiaDigital/project/service/IAvaliacaoFisica.java
@@ -2,6 +2,7 @@ package dio.academiaDigital.project.service;
 
 import dio.academiaDigital.project.entity.AvaliacaoFisica;
 import dio.academiaDigital.project.entity.form.AvaliacaoFisicaForm;
+import dio.academiaDigital.project.entity.form.AvaliacaoFisicaUpdateForm;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface IAvaliacaoFisica {
     AvaliacaoFisica create(AvaliacaoFisicaForm form);
     AvaliacaoFisica get(Long id);
     List<AvaliacaoFisica> getAll();
-//    AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate);
+    AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate);
 
     void delete(Long id);
 }
