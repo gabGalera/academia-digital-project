ac469ff69904549449e1d6d6e250fcef767d7aec
Gabriel Galera <gabgalera@hotmail.com>
2023-04-29 15:22:26 -0300

    feat: IAlunoService interface

---
+++ src/main/java/dio/academiaDigital/project/service/IAlunoService.java
@@ -0,0 +1,12 @@
+package dio.academiaDigital.project.service;
+
+import dio.academiaDigital.project.entity.Aluno;
+
+import java.util.List;
+
+public interface IAlunoService {
+
+//    Aluno create(AlunoForm form);
+    Aluno get(Long id);
+    List<Aluno> getAll();
+}
