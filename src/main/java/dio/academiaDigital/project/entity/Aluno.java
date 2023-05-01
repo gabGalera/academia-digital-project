46ef4c023eb9157db0b3e34202097855d5c9afaa
Gabriel Galera <gabgalera@hotmail.com>
2023-05-01 09:47:26 -0300

    feat: propriedades JPA na classe Aluno

--- src/main/java/dio/academiaDigital/project/entity/Aluno.java
+++ src/main/java/dio/academiaDigital/project/entity/Aluno.java
@@ -1,16 +1,35 @@
 package dio.academiaDigital.project.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Aluno {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String cpf;
     private String bairro;
     private LocalDate dataDeNascimento;
+
+    @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
