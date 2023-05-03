da39a7100f6f0862eecff22f9e552fc728a1fb22
Gabriel Galera <gabgalera@hotmail.com>
2023-05-03 09:49:41 -0300

    fix: arruma restrições nas entities

--- src/main/java/dio/academiaDigital/project/entity/form/AlunoForm.java
+++ src/main/java/dio/academiaDigital/project/entity/form/AlunoForm.java
@@ -17,18 +17,18 @@ import java.time.LocalDate;
 public class AlunoForm {
 
     @NotBlank(message = "Preencha o campo corretamente.")
-    @Size(min = 3, max = 50, message = "${validatedValue} precisa estar entre {min} e {max} caracteres.")
+    @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
     private String nome;
 
     @NotBlank(message = "Preencha o campo corretamente.")
-    @CPF(message = "${validatedValue} é inválido!")
+    @CPF(message = "'${validatedValue}' é inválido!")
     private String cpf;
 
     @NotBlank(message = "Preencha o campo corretamente.")
-    @Size(min = 3, max = 50, message = "${validatedValue} precisa estar entre {min} e {max} caracteres.")
+    @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
     private String bairro;
 
     @NotNull(message = "Preencha o campo corretamente.")
-    @Past(message = "${validatedValue} precisa estar entre {min} e {max} caracteres.")
+    @Past(message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
     private LocalDate dataDeNascimento;
 }
